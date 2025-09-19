2b5bf7ec62a634d5eb280e80d435e5b60de7f09e
Diptipathak2007 <diptipathak019@gmail.com>
2025-09-19 07:16:51 +0530

    Learned about what is require in solidity and used it in our twitter struct

--- contracts/Twitter_struct.sol
+++ contracts/Twitter_struct.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.0;
 //3.Test Tweets
 contract Twitter{
     //define our struct
+    //1.use require syntax to limit the length of the tweet upto 280 characters
+
+    uint16 constant MAX_TWEET_LENGTH=280;
     struct Tweet{
         address author;
         string content;
@@ -14,6 +17,9 @@ contract Twitter{
     //add our code
     mapping (address=>Tweet[])public tweets;
     function createTweet(string memory _tweet)public {
+        //condiitional
+        //if tweet length<=280 then we are good,otherwise we revert
+        require(bytes(_tweet).length<=MAX_TWEET_LENGTH,"Tweet is too long");
         Tweet memory newTweet=Tweet({
             author:msg.sender,
             content:_tweet,
@@ -22,10 +28,16 @@ contract Twitter{
         });
         tweets[msg.sender].push(newTweet);
     }
-    function getTweet(address _owner,uint _i)public view returns(Tweet memory){
-        return tweets[_owner][_i];
+    function getTweet(uint _i)public view returns(Tweet memory){
+        return tweets[msg.sender][_i];
     }
     function getAllTweets(address _owner)public view returns(Tweet[] memory){
         return tweets[_owner];
     }
+    //solidity require statement
+    // function likeTweet(address _owner,uint _i)public{
+    //     require(tweets[_owner][_i].author!=msg.sender,"You cannot like your own tweet");
+    //     tweets[_owner][_i].likes++;
+    // }
+    
 }
